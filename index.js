5432107c03e4b5abfe6ed5d852f21d90ebf8ee11
Eddie Monge <eddie@eddiemonge.com>
2014-07-18 18:28:40 -0700

    code formatting changes

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ var _ = require('lodash');
 var Tree = require('ast-query');
 
 /**
- * A class managing the edition of the project Gruntfile content. Editing the Gruntfile
- * using this class allow easier Generator composability as they can work and add parts
- * to the same Gruntfile without having to parse the Gruntfile AST themselves.
+ * A class managing the edition of the project Gruntfile content. Editing the
+ * Gruntfile using this class allow easier Generator composability as they can
+ * work and add parts to the same Gruntfile without having to parse the
+ * Gruntfile AST themselves.
  * @constructor
- * @param  {String} gruntfileContent - The project's actual Gruntfile.js content
+ * @param  {String} gruntfileContent - The actual `Gruntfile.js` content
  */
 
 var GruntfileEditor = module.exports = function (gruntfileContent) {
-  gruntfileContent = gruntfileContent || fs.readFileSync(path.join(__dirname, 'default-gruntfile.js'));
+  gruntfileContent = gruntfileContent ||
+    fs.readFileSync(path.join(__dirname, 'default-gruntfile.js'));
   this.gruntfile = new Tree(gruntfileContent.toString());
 };
 
 /**
- * Insert a configuration section inside the Gruntfile.js (`grunt.initConfig`)
+ * Insert a configuration section inside the `Gruntfile.js` initConfig() block
  * @param  {String} name   - Key name of the configuration block
  * @param  {String} config - Configuration content code as a string.
  * @return {this}
@@ -28,7 +30,10 @@ var GruntfileEditor = module.exports = function (gruntfileContent) {
 
 GruntfileEditor.prototype.insertConfig = function (name, config) {
   assert(_.isString(name), 'You must provide a task name');
-  assert(_.isString(config), 'You must provide a task configuration body as String');
+  assert(
+    _.isString(config),
+    'You must provide a task configuration body as a String'
+  );
   this.gruntfile.callExpression('grunt.initConfig').arguments.at(0)
     .key(name).value(config);
   return this;
@@ -36,7 +41,7 @@ GruntfileEditor.prototype.insertConfig = function (name, config) {
 
 /**
  * Load a Grunt plugin
- * @param {String}               pluginName - name of the plugin to load (ex: 'grunt-contrib-uglify')
+ * @param {String} pluginName - name of the plugin to load (ex: 'grunt-sass')
  * @return {this}
  */
 
@@ -49,15 +54,16 @@ GruntfileEditor.prototype.loadNpmTasks = function (pluginName) {
 
 /**
  * Register a task inside a named task group
- * @param {String}               name  - Task group name
+ * @param {String} name  - Task group name
  * @param {String|Array[String]} tasks - Tasks name to insert in the group
  * @return {this}
  */
 
 GruntfileEditor.prototype.registerTask = function (name, tasks) {
-  var current = this.gruntfile.callExpression('grunt.registerTask').filter(function (node) {
-    return node.arguments[0].value === name;
-  });
+  var current = this.gruntfile.callExpression('grunt.registerTask')
+    .filter(function (node) {
+      return node.arguments[0].value === name;
+    });
   tasks = _.isArray(tasks) ? tasks : [tasks];
   if (!current.length) {
     this.gruntfile.assignment('module.exports').value().body.append(
