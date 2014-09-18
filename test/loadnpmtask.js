9132334bb5003b265476f064fc5f4716c8f676ab
EdoPut <edoardo.putti@gmail.com>
2014-09-18 20:16:26 +0200

    modified loadNpmTasks to work with strings and arrays as an input, added unit tests

--- test/loadnpmtask.js
+++ test/loadnpmtask.js
@@ -19,36 +19,43 @@ describe('#loadNpmTask()', function () {
   describe('requires a name', function () {
     var msg = /You must provide a plugin name/;
 
-    it('as a String', function () {
+    it('as a string or array', function () {
       assert(this.load('name'));
       assert(this.load(' name '));
+      assert(this.load(['foo', 'bar']));
+      assert(this.load(['foo    ', '   bar   ']));
       assert.throws(this.load(), msg);
       assert.throws(this.load(''), msg);
       assert.throws(this.load(' '), msg);
+      assert.throws(this.load([]), msg);
+      assert.throws(this.load(['']), msg);
+      assert.throws(this.load([{}]), msg);
       assert.throws(this.load({}), msg);
       assert.throws(this.load(42), msg);
       assert.throws(this.load(false), msg);
       assert.throws(this.load(true), msg);
       assert.throws(this.load(0), msg);
       assert.throws(this.load(-1), msg);
-      assert.throws(this.load([ 'an', 'array' ]), msg);
     });
   });
 
   describe('inserts', function () {
-    var loadTask = 'grunt.loadNpmTasks(\'grunt-contrib-concat\')';
-
-    it('insert task plugin load if not existing', function () {
+    var loadTask = 'grunt.loadNpmTasks(\'grunt-contrib-concat\');';
+    it('insert task plugin from string if not existing', function () {
       assert(this.str().indexOf(loadTask) < 0);
       this.editor.loadNpmTasks('  grunt-contrib-concat  ');
+      assert(this.str().indexOf(loadTask) >= 0);
+    });
+    it('insert task plugin from array if not existing', function () {
+      assert(this.str().indexOf(loadTask) < 0);
+      this.editor.loadNpmTasks(['  grunt-contrib-concat  ']);
       assert(this.str().indexOf(loadTask) >= 0);
     });
 
     it('ignore duplicate loads', function () {
       assert(this.str().indexOf(loadTask) < 0);
-      this.editor.loadNpmTasks('grunt-contrib-concat  ');
-      this.editor.loadNpmTasks(' grunt-contrib-concat');
-      assert(this.str().indexOf(loadTask) >= 0);
+      this.editor.loadNpmTasks(['grunt-contrib-concat  ',' grunt-contrib-concat']);
+      assert(this.str().indexOf(loadTask) === this.str().lastIndexOf(loadTask));
     });
   });
 });
